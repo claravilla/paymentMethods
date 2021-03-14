f53069d935b3687e9c16644b765b262bc9af7a3c
David Vekony <68352412+davidvekony@users.noreply.github.com>
2021-03-14 22:10:52 +0100

    prettified code

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function paymentMethod (payment) {
-    switch (payment) {
-        case "credit":
-            alert("Good choice - We'll take you to checkout");
-        break;
-        
-        case "apple":
-        alert("If you insist");
-            break;
+function paymentMethod(payment) {
+  switch (payment) {
+    case "credit":
+      alert("Good choice - We'll take you to checkout");
+      break;
 
-        case "paypal": 
-            alert("If you have something in backorder it will cause you grief");
-                break;
-        case "invoice":
-        alert("This is the 21st century. Choose a modern payment method");
-            break;
-    }
+    case "apple":
+      alert("If you insist");
+      break;
+
+    case "paypal":
+      alert("If you have something in backorder it will cause you grief");
+      break;
+    case "invoice":
+      alert("This is the 21st century. Choose a modern payment method");
+      break;
+  }
 }
 
 /*
